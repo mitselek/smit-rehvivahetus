364385d32473a077b25c55797b49ab89a48d03c4
michelek <mihkel.putrinsh@gmail.com>
2025-02-28 09:37:26 +0100

    # Run tests once npm test
    # Run tests in watch mode (will re-run when files change)
    npm run test:watch

---
+++ jest.setup.js
@@ -0,0 +1,21 @@
+// Mock window.scrollTo
+window.scrollTo = jest.fn()
+
+// Mock FormData
+global.FormData = class FormData {
+    constructor(form) {
+        this.data = {}
+    }
+    
+    append(key, value) {
+        this.data[key] = value
+    }
+    
+    get(key) {
+        return this.data[key]
+    }
+    
+    forEach(callback) {
+        Object.entries(this.data).forEach(([key, value]) => callback(value, key))
+    }
+}
